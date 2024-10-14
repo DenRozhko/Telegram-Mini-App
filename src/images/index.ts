11c3a36b141155f5356cea0d3d0474dc24038604
DenRozhko <DenRozhko@users.noreply.github.com>
2024-10-14 22:03:15 +0300

    Update index.ts

--- src/images/index.ts
+++ src/images/index.ts
@@ -4,7 +4,7 @@ import dollarCoin from "./dollar-coin.png";
 import dailyReward from "./daily-reward.png";
 import dailyCipher from "./daily-cipher.png";
 import dailyCombo from "./daily-combo.png";
-import mainCharacter from "./girl2.png";
+import mainCharacter from "./girl4.png";
 import hamsterCoin from "./hamster-coin.png";
 
 export {
